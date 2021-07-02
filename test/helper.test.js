978f34fd933ec0f1c74f8677614d01d6c1fafad0
Adrien Zinger <azinger@alpenite.com>
2021-07-02 15:04:07 +0200

    Unit test filter by param

--- test/helper.test.js
+++ test/helper.test.js
@@ -13,3 +13,17 @@ test('Check if we can get by tag 1', () => {
     }
   ])
 })
+
+test('Check if we can get by param 1', () => {
+  const content = `<gu ok="one" nok="two"><ma ok="three" nok="four"></ma></gu>`
+  const js = toJs(content)
+  const res = js.findByParam('ok', 'three')
+  expect(res).toStrictEqual([
+    {
+      params: { ok: 'three', nok: 'four' },
+      xmlTag: '<ma ok="three" nok="four">',
+      tag: 'ma',
+      children: []
+    }
+  ])
+})

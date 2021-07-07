f646e43f0ee81e377355d5fd0f5c5d2e2be8a70c
Adrien Zinger <zinger.ad@gmail.com>
2021-07-07 11:40:27 +0200

    Add a path tag filter

--- test/helper.test.js
+++ test/helper.test.js
@@ -27,3 +27,83 @@ test('Check if we can get by param 1', () => {
     }
   ])
 })
+
+test('Check if we can get by path 1', () => {
+  const content = `<a><b><c>the graal</c></b><b></b></a>`
+  const js = toJs(content)
+  const res = js.findByPath(['a', 'b', 'c'])
+  expect(res).toStrictEqual([
+    {
+      params: {},
+      xmlTag: '<c>',
+      tag: 'c',
+      children: [
+        {
+          params: {},
+          xmlTag: '',
+          tag: '',
+          children: [],
+          text: 'the graal'
+        }
+      ]
+    }
+  ])
+})
+
+test('Check if we can get by path 2', () => {
+  const content = `<a><c>not this</c><b><c>the graal</c></b><b><c>the graal again !</c></b></a>`
+  const js = toJs(content)
+  const res = js.findByPath(['a', 'b', 'c'])
+  expect(res).toStrictEqual([
+    {
+      params: {},
+      xmlTag: '<c>',
+      tag: 'c',
+      children: [
+        {
+          params: {},
+          xmlTag: '',
+          tag: '',
+          children: [],
+          text: 'the graal again !'
+        }
+      ]
+    },
+    {
+      params: {},
+      xmlTag: '<c>',
+      tag: 'c',
+      children: [
+        {
+          params: {},
+          xmlTag: '',
+          tag: '',
+          children: [],
+          text: 'the graal'
+        }
+      ]
+    }
+  ])
+})
+
+test('Check if we can get by path 3', () => {
+  const content = `<a><c>this</c><b><c>not the graal</c></b><b><c>not the graal again !</c></b></a>`
+  const js = toJs(content)
+  const res = js.findByPath(['a', 'c'])
+  expect(res).toStrictEqual([
+    {
+      params: {},
+      xmlTag: '<c>',
+      tag: 'c',
+      children: [
+        {
+          params: {},
+          xmlTag: '',
+          tag: '',
+          children: [],
+          text: 'this'
+        }
+      ]
+    }
+  ])
+})

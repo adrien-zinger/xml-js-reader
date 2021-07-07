992ea61f39c4dbe8e939934a7a855fc26de25c7d
Adrien Zinger <azinger@alpenite.com>
2021-07-07 10:52:25 +0200

    Parsing text between balises

--- test/parsing.test.js
+++ test/parsing.test.js
@@ -1,7 +1,6 @@
 const {
   getParam,
   mapChildrenStr,
-  toJs,
 } = require('../src/internal/parsingTools')
 
 test('Check if we can get a inline object', () => {
@@ -42,7 +41,7 @@ test('Check if we can get childen str 1', () => {
   const children = '<mo bar="ko" ma="ko"></mo>'
   const content = `<gu ok="one" nok="two">${children}</gu>`
   let params = getParam(content)
-  const res = mapChildrenStr(params.xmlTag, content, (child) => child)
+  const res = mapChildrenStr(params.xmlTag, content, ({ xml_pop_object }) => xml_pop_object)
   expect(res.join('')).toBe(children)
 })
 
@@ -50,7 +49,7 @@ test('Check if we can get childen str 2', () => {
   const children = '<mo bar="ko" ma="ko"><na/></mo>'
   const content = `<gu ok="one" nok="two"> ${children}  </gu>`
   let params = getParam(content)
-  const res = mapChildrenStr(params.xmlTag, content, (child) => child)
+  const res = mapChildrenStr(params.xmlTag, content, ({ xml_pop_object }) => xml_pop_object)
   expect(res.join('')).toBe(children)
 })
 
@@ -58,7 +57,7 @@ test('Check if we can get childen str 3', () => {
   const children = ''
   const content = `<gu ok="one" nok="two"/>`
   let params = getParam(content)
-  const res = mapChildrenStr(params.xmlTag, content, (child) => child)
+  const res = mapChildrenStr(params.xmlTag, content, ({ xml_pop_object }) => xml_pop_object)
   expect(res.join('')).toBe(children)
 })
 
@@ -66,7 +65,7 @@ test('Check if we can get childen str 4', () => {
   const children = ''
   const content = `<gu ok="one" nok="two"   />`
   let params = getParam(content)
-  const res = mapChildrenStr(params.xmlTag, content, (child) => child)
+  const res = mapChildrenStr(params.xmlTag, content, ({ xml_pop_object }) => xml_pop_object)
   expect(res.join('')).toBe(children)
 })
 
@@ -75,7 +74,7 @@ test('Check if we can get childen str 5', () => {
     '<mo bar="ko" ma="ko"><na/></mo><mo bar="ko" ma="ko"><na/></mo>'
   const content = `<gu ok="one" nok="two" > ${children} </gu>`
   let params = getParam(content)
-  const res = mapChildrenStr(params.xmlTag, content, (child) => child)
+  const res = mapChildrenStr(params.xmlTag, content, ({ xml_pop_object }) => xml_pop_object)
   expect(res.join('')).toBe(children)
 })
 
@@ -84,7 +83,7 @@ test('Check if we can get childen str 6', () => {
     '<mo bar="ko" ma="ko"><gu>fds</gu></mo><mo bar="ko" ma="ko"><na/></mo>'
   const content = `<gu ok="one" nok="two" > ${children} </gu><mama>nok</mama>`
   let params = getParam(content)
-  const res = mapChildrenStr(params.xmlTag, content, (child) => child)
+  const res = mapChildrenStr(params.xmlTag, content, ({ xml_pop_object }) => xml_pop_object)
   expect(res.join('')).toBe(children)
 })
 
@@ -92,35 +91,6 @@ test('Check if we can get childen str 7', () => {
   const children = ''
   const content = `<gu ok="one" nok="two"></gu>`
   let params = getParam(content)
-  const res = mapChildrenStr(params.xmlTag, content, (child) => child)
+  const res = mapChildrenStr(params.xmlTag, content, ({ xml_pop_object }) => xml_pop_object)
   expect(res.join('')).toBe(children)
-})
-
-test('Check if we can transform to js 1', () => {
-  const content = `<gu ok="one" nok="two"/>`
-  const js = toJs(content)
-  expect(js).toStrictEqual({
-    tag: 'gu',
-    xmlTag: content,
-    params: { ok: 'one', nok: 'two' },
-    children: [],
-  })
-})
-
-test('Check if we can transform to js 2', () => {
-  const content = `<gu ok="one" nok="two"><ma ok="three" nok="four"></ma></gu>`
-  const js = toJs(content)
-  expect(js).toStrictEqual({
-    tag: 'gu',
-    xmlTag: '<gu ok="one" nok="two">',
-    params: { ok: 'one', nok: 'two' },
-    children: [
-      {
-        tag: 'ma',
-        xmlTag: '<ma ok="three" nok="four">',
-        params: { ok: 'three', nok: 'four' },
-        children: [],
-      },
-    ],
-  })
 })
---
+++ test/toJs.test.js
@@ -0,0 +1,141 @@
+const { toJs } = require("../src/internal/parsingTools")
+
+test('Check if we can transform to js 1', () => {
+  const content = `<gu ok="one" nok="two"/>`
+  const js = toJs(content)
+  expect(js).toStrictEqual({
+    tag: 'gu',
+    xmlTag: content,
+    params: { ok: 'one', nok: 'two' },
+    children: [],
+  })
+})
+
+test('Check if we can transform to js 2', () => {
+  const content = `<gu ok="one" nok="two"><ma ok="three" nok="four"></ma></gu>`
+  const js = toJs(content)
+  expect(js).toStrictEqual({
+    tag: 'gu',
+    xmlTag: '<gu ok="one" nok="two">',
+    params: { ok: 'one', nok: 'two' },
+    children: [
+      {
+        tag: 'ma',
+        xmlTag: '<ma ok="three" nok="four">',
+        params: { ok: 'three', nok: 'four' },
+        children: [],
+      },
+    ],
+  })
+})
+
+test('Check if we can transform to js 3', () => {
+  const content = `<gu ok="one" nok="two">hello<ma ok="three" nok="four"></ma></gu>`
+  const js = toJs(content)
+  expect(js).toStrictEqual({
+    tag: 'gu',
+    xmlTag: '<gu ok="one" nok="two">',
+    params: { ok: 'one', nok: 'two' },
+    children: [
+      {
+        params: {},
+        tag: '',
+        xmlTag: '',
+        text: 'hello',
+        children: [],
+      },
+      {
+        tag: 'ma',
+        xmlTag: '<ma ok="three" nok="four">',
+        params: { ok: 'three', nok: 'four' },
+        children: [],
+      },
+    ],
+  })
+})
+
+test('Check if we can transform to js 4', () => {
+  const content = `<gu ok="one" nok="two">hello<ma ok="three" nok="four">hello2</ma>hello3</gu>`
+  const js = toJs(content)
+  expect(js).toStrictEqual({
+    tag: 'gu',
+    xmlTag: '<gu ok="one" nok="two">',
+    params: { ok: 'one', nok: 'two' },
+    children: [
+      {
+        params: {},
+        tag: '',
+        xmlTag: '',
+        text: 'hello',
+        children: [],
+      },
+      {
+        tag: 'ma',
+        xmlTag: '<ma ok="three" nok="four">',
+        params: { ok: 'three', nok: 'four' },
+        children: [
+          {
+            params: {},
+            tag: '',
+            xmlTag: '',
+            text: 'hello2',
+            children: [],
+          },
+        ],
+      },
+      {
+        params: {},
+        tag: '',
+        xmlTag: '',
+        text: 'hello3',
+        children: [],
+      },
+    ],
+  })
+})
+
+test('Check if we can transform to js 5', () => {
+  const content = `<gu ok="one" nok="two">
+    \n\r hello<ma ok="three" nok="four">
+      \t\n\r\thello2
+      
+      
+      \n\r</ma>\nhello3
+        \t\n\r</gu>`
+  const js = toJs(content)
+  expect(js).toStrictEqual({
+    tag: 'gu',
+    xmlTag: '<gu ok="one" nok="two">',
+    params: { ok: 'one', nok: 'two' },
+    children: [
+      {
+        params: {},
+        tag: '',
+        xmlTag: '',
+        text: 'hello',
+        children: [],
+      },
+      {
+        tag: 'ma',
+        xmlTag: '<ma ok="three" nok="four">',
+        params: { ok: 'three', nok: 'four' },
+        children: [
+          {
+            params: {},
+            tag: '',
+            xmlTag: '',
+            text: 'hello2',
+            children: [],
+          },
+        ],
+      },
+      {
+        params: {},
+        tag: '',
+        xmlTag: '',
+        text: 'hello3',
+        children: [],
+      },
+    ],
+  })
+})
